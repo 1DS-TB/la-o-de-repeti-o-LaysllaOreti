25549578ea1eb8901e95d36ac30b934c16e609fe
Layslla Eduarda Oreti dos Santos <116527092+LaysllaOreti@users.noreply.github.com>
2025-04-16 15:56:08 -0300

    Atualização exercício 6

--- exercicio6.py
+++ exercicio6.py
@@ -1,11 +1,14 @@
-#Solicita a quantidade desejada
-N = int(input("Digite a quantidade de termos da sequência de Fibonacci: "))
+num = int(input("Digite quantos termos você deseja: "))
+a = 1
+b = 0
+contador = 0
+fibonnaci = []
 
-#Inicializa od dois primeiros termos
-a, b = 0, 1
-
-print("Sequência de Fibonacci:")
-
-for i in range(N):
-    print(a, end=", " if i < N - 1 else "\n")
-    a, b = b, a + b
+if num < 1:
+    print("INVALIDO")
+else:
+    while contador < num:
+        a,b = b,a+b
+        fibonnaci.append(a)
+        contador +=1
+print(fibonnaci)
