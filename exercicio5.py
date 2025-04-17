0fc58451b8a96264319b5746fa173108a69b90a5
Layslla Eduarda Oreti dos Santos <116527092+LaysllaOreti@users.noreply.github.com>
2025-04-17 07:47:55 -0300

    Atualização exercício 5

--- exercicio5.py
+++ exercicio5.py
@@ -1,13 +1,13 @@
-numb = int(input("Digite um número: "))
+num = int(input("Digite um número: "))
 divisor = 0
 
 for i in range(1, num +1):
-    if numb % i == 0 and num >= 1:
+    if num % i == 0 and num >= 1:
         divisor +=1
 
 if divisor == 2:
-    print(f"{numb} eh primo")
+    print(f"{num} eh primo")
 elif divisor <1:
     print("INVALIDO")
 else:
-    print(f"{numb} nao eh primo")
+    print(f"{num} nao eh primo")
